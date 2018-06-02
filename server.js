52608423c8101a602da6d72c6607f5d88f59a8fe
Hatem <hattoum96@gmail.com>
2018-06-03 01:48:43 +0200

    wot

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express()
 setInterval(function() {
     http.get("https://calm-mesa-88721.herokuapp.com/webhook");
     console.log("hmmm");
-}, 300000); // every 5 minutes (300000)
+}, 10000); // every 5 minutes (300000)
 
 
 app.listen(app.get('port'), function() {
